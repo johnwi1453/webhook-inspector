91f519d940208b2a7794ca1a9b316e234659658a
john williams <johnwi1453@johns-MacBook-Air.local>
2025-07-04 18:23:23 -0700

    logging test

--- backend/main.go
+++ backend/main.go
@@ -62,7 +62,10 @@ func main() {
 
 	// Serve dist folder
 	fs := http.StripPrefix("/", http.FileServer(http.Dir("./frontend/dist")))
-	r.Handle("/*", fs)
+	r.Handle("/*", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Frontend fallback hit for path:", r.URL.Path)
+		fs.ServeHTTP(w, r)
+	}))
 
 	log.Println("Starting server on :8080")
 	if err := http.ListenAndServe(":8080", r); err != nil {
