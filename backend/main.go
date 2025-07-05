9df2d1e8854422f43957dca068a8ff2c1abd16d3
john williams <johnwi1453@johns-MacBook-Air.local>
2025-07-04 18:20:22 -0700

    Strip prefix

--- backend/main.go
+++ backend/main.go
@@ -61,8 +61,8 @@ func main() {
 	})
 
 	// Serve dist folder
-	fs := http.FileServer(http.Dir("./frontend/dist"))
-	r.Handle("/*", fs) // fallback route to frontend
+	fs := http.StripPrefix("/", http.FileServer(http.Dir("./frontend/dist")))
+	r.Handle("/*", fs)
 
 	log.Println("Starting server on :8080")
 	if err := http.ListenAndServe(":8080", r); err != nil {
